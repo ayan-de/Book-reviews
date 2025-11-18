d6b504b5cdb43a7164bb0a1a338edec63b16c164
Ayan De <deayan252@gmail.com>
2025-11-18 17:39:51 +0530

    added some query

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -16,5 +19,22 @@ class Book extends Model
     public function reviews(): HasMany
     {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where("title","like","%". $title ."%");
+    }
+
+    public function scopePopular(Builder $query): Builder|QueryBuilder
+    {
+        return $query->withCount('reviews')
+        ->orderBy('reviews_count','desc');
+    }
+
+    public function scopeHighestRated(Builder $query): Builder|QueryBuilder
+    {
+        return $query->withAvg('reviews','rating')
+        ->orderBy('reviews_avg_rating','desc');
     }
 }
