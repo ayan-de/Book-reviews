bd2fc33bb18816ace758ec06188f2e88a48fc7ba
Ayan De <deayan252@gmail.com>
2025-11-18 16:35:03 +0530

    Added db seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Book;
+use App\Models\Review;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -21,5 +23,32 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5,30);
+
+            Review::factory()->count($numReviews)
+            ->good()
+            ->for($book)
+            ->create();
+        });
+
+        Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5,30);
+
+            Review::factory()->count($numReviews)
+            ->average()
+            ->for($book)
+            ->create();
+        });
+
+        Book::factory(34)->create()->each(function ($book) {
+            $numReviews = random_int(5,30);
+
+            Review::factory()->count($numReviews)
+            ->bad()
+            ->for($book)
+            ->create();
+        });
     }
 }
